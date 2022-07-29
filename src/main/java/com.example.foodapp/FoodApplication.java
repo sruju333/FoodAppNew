f383c20f080b75fce3c8f4d50864673f563d2f31
sruju333 <sruju333@gmail.com>
2022-07-29 10:20:57 +0530

    Added System.properties

--- src/main/java/com.example.foodapp/FoodApplication.java
+++ src/main/java/com.example.foodapp/FoodApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import javax.persistence.Entity;
-
 @SpringBootApplication
 public class FoodApplication {
     public static void main(String[] args) {
