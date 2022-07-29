dbf8cdc9b5fd5a42c75fde5daecd326a89aab928
akshat1-agrawal <akshat.agrawal@olacabs.com>
2022-07-29 12:25:53 +0530

    Added product Details API

--- src/main/java/com.example.foodapp/controller/ProductController.java
+++ src/main/java/com.example.foodapp/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.foodapp.controller;
 import com.example.foodapp.model.entities.Product;
 import com.example.foodapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +20,10 @@ public class ProductController {
     @PostMapping("/product/update")
     public String updateProduct(@RequestBody Product product){
         return productService.updateProduct(product).getMessage();
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<?> searchProduct(@PathVariable Long id){
+        return ResponseEntity.ok(productService.searchProduct(id));
     }
 }
